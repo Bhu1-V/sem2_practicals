e630ad12eae456e8bd610a4af60e9bb938343a51
pWNn1sher <70578657+pWNn1sher@users.noreply.github.com>
2020-11-15 18:13:09 +0530

    implemented successfully

--- 12_dynamic_memory.cpp
+++ 12_dynamic_memory.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
 
+class object{
+    public:
+    object(){
+        cout<<"Object Instance Created\n\n";
+    };
+    ~object(){
+        cout<<"Object Instance Destroyed\n\n";
+    }
+};
+
+void create_an_object(){
+    object* instance_pointer = new object();
+    cout<<">> Created but never Destroyed \n\n";
+}
+
+void create_and_destroy_object(){
+    object* instance_pointer = new object();
+    cout<<">> Using Object Instance.\n\n";
+    delete instance_pointer;
+}
+
 int main(){
-    int* a = new int[10];
     
+    create_an_object();
+    cout<<">> Let's Do It Properly.\n\n";
+    create_and_destroy_object();
 
     return 0;
 }
